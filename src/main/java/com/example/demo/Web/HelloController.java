16e1d49dbb361ae5ba9825c26e55832c303d8b59
ha <yue11se@163.com>
2020-09-28 22:51:07 +0800

    第一次

---
+++ src/main/java/com/example/demo/Web/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String index() {
+        System.out.println("主修改1");
+        return "Hello World";
+    }
+}
